d091ac18ef311b14a42facf7bd1831fd0af83b60
Lucas Benevenute <souzalucas6@hotmail.com>
2023-05-06 13:22:19 -0300

    Att

---
+++ Lista de Exercícios 1 - Lucas de Souza Benevenute/18.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+int num;
+   
+   printf("Digite um numero inteiro: ");
+   scanf("%d", &num);
+   
+   for (int i = 1; i <= num; i++) {
+      if (num % i == 0) {
+         printf("%d\n", i);
+      }
+   }
+}
